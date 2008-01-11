aa8801b261ef093948b352188c2d842e995c0e24
Matt Mitchell <mwm4n@virginia.edu>
2008-01-11 13:32:13 +0000

    Moved things around for the Quandu env

---
+++ solr-home/post.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+require '/sds/shared/deployer-lib/ruby/files_from_mdate'
+require '/sds/shared/deployer-lib/ruby/solr_update'
+
+HOST = 'http://localhost:8080'
+
+mdate = ARGV[0].nil? ? nil : ARGV[0].to_sym
+update = SolrUpdate.new(HOST + '/womensbios-solr')
+update.index( File.join(File.dirname(__FILE__), 'add_docs', 'add_*.xml'), mdate)
