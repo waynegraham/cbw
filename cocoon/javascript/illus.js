e248b0c10f26095a6d8d01bddc26d0b02c5bec21
Joe Gilbert <jfg9x@virginia.edu>
2008-05-23 20:00:05 +0000

    Trying to pop illustration without caption as a test.

--- cocoon/javascript/illus.js
+++ cocoon/javascript/illus.js
@@ -1,10 +1,10 @@
-function displayIllus(illus, caption)
+function displayIllus(illus)
 {
 	var illusWin=window.open('','illustration','height=400,width=500');
 	illusWin.document.write('<html><head><title>The Collective Biographies of Women: Illustration</title>');
 	illusWin.document.write("<link type='text/css' href='style.css' rel='stylesheet'/>)")
 	illusWin.document.write('</head><body>');
-	illusWin.document.write("<h3>" + caption + "</h3>");
+	illusWin.document.write("<h3>Test</h3>");
 	illusWin.document.write("<img src='full/" + illus + "'/>");
 	illusWin.document.write('<p><a href="javascript:self.close()">Close</a> the popup.</p>');
 	illusWin.document.write('</body></html>');
