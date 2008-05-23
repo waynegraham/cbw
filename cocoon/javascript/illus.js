65478f3d8d738d8d046d32b5d77cf095a99fa7f7
Joe Gilbert <jfg9x@virginia.edu>
2008-05-23 21:42:16 +0000

    Minor tweaks to illustration pop-up.

--- cocoon/javascript/illus.js
+++ cocoon/javascript/illus.js
@@ -1,13 +1,13 @@
 function displayIllus(illus, caption)
 {
-  var generator=window.open('','name','height=800,width=800px, resizable=1');
+  var generator=window.open('','name','height=800,width=800px, scrollbars resizable=1');
   
   generator.document.write('<html><head><title>Popup</title>');
   generator.document.write('<link rel="stylesheet" href="style.css">');
   generator.document.write('</head><body>');
-  generator.document.write('<h3>' + unescape(caption) + '</h3>');
+  generator.document.write('<h3 class=\'caption\'>' + unescape(caption) + '</h3>');
   generator.document.write('<img src=\"full/' + illus + '\">');
-  generator.document.write('<p><a href="javascript:self.close()"> Close</a> this window.</p>');
+  generator.document.write('<p class=\'winClose\'><a href="javascript:self.close()"> Close</a> this window.</p>');
   generator.document.write('</body></html>');
   generator.document.close();
 }
