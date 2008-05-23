f164156bb3e1667ba8084ef49c38aece45dce1d5
Joe Gilbert <jfg9x@virginia.edu>
2008-05-23 19:52:03 +0000

    syntax error in js call.

--- cocoon/javascript/illus.js
+++ cocoon/javascript/illus.js
@@ -1,8 +1,8 @@
 function displayIllus(illus, caption)
 {
-	var illusWin=window.open('','illusWin','height=400,width=500');
+	var illusWin=window.open('','illustration','height=400,width=500');
 	illusWin.document.write('<html><head><title>The Collective Biographies of Women: Illustration</title>');
-	illus.Win.document.write("<link type='text/css' href='style.css' rel='stylesheet'/>)")
+	illusWin.document.write("<link type='text/css' href='style.css' rel='stylesheet'/>)")
 	illusWin.document.write('</head><body>');
 	illusWin.document.write("<h3>" + caption + "</h3>");
 	illusWin.document.write("<img src='full/" + illus + "'/>");
