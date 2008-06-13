dea8751fbf118735638f6fa43136e555ef25d2ae
Joe Gilbert <jfg9x@virginia.edu>
2008-06-13 00:00:47 +0000

    illustration tweaks (divs, not tables, now)

--- cocoon/javascript/illus.js
+++ cocoon/javascript/illus.js
@@ -5,9 +5,9 @@ function displayIllus(illus, caption)
   generator.document.write('<html><head><title>Popup</title>');
   generator.document.write('<link rel="stylesheet" href="style.css">');
   generator.document.write('</head><body>');
-  generator.document.write('<h3 class=\'caption\'>' + unescape(caption) + '</h3>');
+  generator.document.write('<div class=\'head1\'>' + unescape(caption) + '</div>');
   generator.document.write('<img src=\"full/' + illus + '\">');
-  generator.document.write('<p class=\'winClose\'><a href="javascript:self.close()"> Close</a> this window.</p>');
+  generator.document.write('<div class=\'head2\'><a href="javascript:self.close()"> Close</a> this window.</div>');
   generator.document.write('</body></html>');
   generator.document.close();
 }
