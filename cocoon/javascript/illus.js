96315214b565aed65d9761e5f8231fca8aba2053
Joe Gilbert <jfg9x@virginia.edu>
2009-09-22 22:18:45 +0000

    fixing lightboxes?

--- cocoon/javascript/illus.js
+++ cocoon/javascript/illus.js
@@ -1,17 +1,11 @@
 function displayIllus(illus, caption)
 {
   var generator=window.open('','name','height=700px,width=960px, scrollbars=1, resizable=1');
-  
-  generator.document.write('<html><head><title>Popup</title>');
-  generator.document.write('<link rel="stylesheet" href="style.css">');
-  generator.document.write('</head><body id=\'image-wrap\'>');
   cap = (unescape(caption));
   cap = cap.replace(/hi rend="italics"/gi, "em");
-  cap = cap.replace(/\/hi/gi, "/em");
-  generator.document.write('<div class=\'head1\'>' + cap  + '</div>');
-  generator.document.write('<img src=\"full/' + illus + '.jpg\">');
-  generator.document.write('<div class=\'head2\'><a href="javascript:self.close()"> Close this window</a>.</div>');
-  generator.document.write('</body></html>');
+  cap = cap.replace(/\/hi/gi, "/em");  
+  
+  generator.document.write('<html><head><title>Image</title><link rel="stylesheet" href="style.css"></head><body id=\'image-wrap\'><div class=\'head1\'>' + cap  + '</div><img src=\"full/' + illus + '.jpg\"><div class=\'head2\'><a href="javascript:self.close()">Close this window</a>.</div></body></html>');
   generator.document.replace();
   generator.document.close();
 }
